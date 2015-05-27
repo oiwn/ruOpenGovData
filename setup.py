c4e3b88f67c2ed87cd4ec4dfe0f266aca98ec7fd
istinspring <istinspring@gmail.com>
2015-05-27 17:10:19 +0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='ruogd',
+    version='0.1',
+    description='Crawlers to scrape Russian Open Government Data',
+    url='https://github.com/istinspring/ruOpenGovData',
+    author='Alex Istinspring',
+    author_email='istinspring@gmail.com',
+    license='GNU 2.0',
+    packages=['ruogd'],
+    zip_safe=False
+)
